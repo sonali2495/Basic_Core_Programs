7365692ad4ec9937b7022e5175628c2e4b69af43
unknown <sonaligadge24@gmail.comgit config --global user.name sonali2495>
2021-08-26 03:33:28 +0530

    Updated code for largest of 3 numbers

---
+++ Largest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.Basic_Core_Programs;
+
+import java.util.Scanner;
+
+public class Largest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value of a, b and c");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if (a >= b && a >= c)
+			System.out.println(a + " is the largest Number");
+
+		else if (b >= a && b >= c)
+			System.out.println(b + " is the largest Number");
+		else
+			System.out.println(c + " is the largest number");
+	}
+}
